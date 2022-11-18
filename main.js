031e05504f38e85a0900e33fe273ea40dcbb1233
AhlamKhalid <ahlamkarkashan@gmail.com>
2022-11-18 08:39:15 +0300

    display products

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const cartBtn = document.getElementById("cart-btn");
 const cartSidebar = document.getElementById("cart-sidebar");
 const closeBtn = document.getElementById("close-btn");
+const cartProducts = document.getElementById("cart-products");
 
 cartBtn.addEventListener("click", () => {
   cartSidebar.classList.add("show");
@@ -27,4 +28,41 @@ const getProducts = async () => {
   productsArray = data.products;
 };
 
-getProducts();
+// display products
+const displayProducts = () => {
+  let productsHTML = "";
+  productsArray.forEach((product) => {
+    productsHTML += `
+        <div class="product">
+          <div class="details">
+            <img src=${product.img} alt=${product.name} class="product-img" />
+            <div class="product-info">
+              <p class="product-name">${product.name}</p>
+              <p class="product-price">$${product.price}</p>
+            </div>
+          </div>
+          <div class="controls">
+            <div class="quantity">
+              <button class="decrement-btn">
+                <i class="fa-solid fa-minus fa-sm"></i>
+              </button>
+              <p class="quantity-num">${product.quantity}</p>
+              <button class="increment-btn">
+                <i class="fa-solid fa-plus fa-sm"></i>
+              </button>
+            </div>
+            <button class="trash-btn">
+              <i class="fa-solid fa-trash-can fa-lg"></i>
+            </button>
+          </div>
+        </div>
+    `;
+  });
+
+  cartProducts.innerHTML = productsHTML;
+};
+
+// after fetching data
+getProducts().then(() => {
+  displayProducts();
+});
